2a12b1be5d734038c697e40e76937b5d5d7bb2ae
husam alsayed <h.alsayedahmad@simulalab.org>
2022-12-22 21:01:36 +0300

    updating website user settings

---
+++ custom-addons/website_extension/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Website Extension',
+    'sequence': 0,
+    'version': '16.0',
+    'summary': 'extension on the website level',
+    'description': 'Website Extension',
+    'category': 'website',
+    'depends': ['website'],
+    'data': [
+        'views/user_settings.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+
+
+
+
+
+}
